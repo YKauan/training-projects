4c88acd1155a2ecb230dfbe5820394722e3194b2
Kauan <00kscn@gmail.com>
2024-08-08 09:37:35 -0300

    Memoteca Project

---
+++ Angular/memoteca/src/app/components/app-cabecalho/app-cabecalho.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cabecalho',
+  templateUrl: './app-cabecalho.component.html',
+  styleUrls: ['./app-cabecalho.component.css']
+})
+export class CabecalhoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
