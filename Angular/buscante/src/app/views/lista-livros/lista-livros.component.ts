2380946e25f348a91dd53fe5e21d60300c3f9eba
Kauan <00kscn@gmail.com>
2024-08-14 10:16:02 -0300

    Buscante v0.0.2

---
+++ Angular/buscante/src/app/views/lista-livros/lista-livros.component.ts
@@ -0,0 +1,50 @@
+import { switchMap, map, tap, filter, debounceTime, throwError, catchError, EMPTY, of } from 'rxjs';
+import { Component } from '@angular/core';
+import { LivroService } from 'src/app/service/livro.service';
+import { Item, LivrosResultado } from 'src/app/models/interfaces';
+import { LivroVolumeInfo } from 'src/app/models/livroVolumeInfo';
+import { FormControl } from '@angular/forms';
+
+const PAUSA = 300;
+
+@Component({
+  selector: 'app-lista-livros',
+  templateUrl: './lista-livros.component.html',
+  styleUrls: ['./lista-livros.component.css']
+})
+export class ListaLivrosComponent {
+
+  campoBusca = new FormControl();
+  mensagemErro = ''
+  livrosResultado: LivrosResultado;
+
+  constructor(private service: LivroService) { }
+
+  livrosEncontrados$ = this.campoBusca.valueChanges
+    .pipe(
+      debounceTime(PAUSA),
+      filter((valorDigitado) => valorDigitado.length >= 3),
+      switchMap((valorDigitado) => this.service.buscar(valorDigitado)),
+      map(resultado => this.livrosResultado = resultado),
+      map(resultado => resultado.items ?? []),
+      map((items) => this.livrosResultadoParaLivros(items)),
+      catchError((erro) => {
+        console.log(erro)
+        return throwError(() => new Error(this.mensagemErro ='Ops, ocorreu um erro. Recarregue a aplicação!'))
+      })
+    )
+
+  livrosResultadoParaLivros(items: Item[]): LivroVolumeInfo[] {
+    return items.map(item => {
+      return new LivroVolumeInfo(item)
+    })
+  }
+
+}
+
+
+
+function trhowError(arg0: () => Error) {
+  throw new Error('Function not implemented.');
+}
+
